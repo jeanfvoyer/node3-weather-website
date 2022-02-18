ac09a2e856e2a2606ca33cf2e23bcf963ec9bf38
JFVOYERPC\jeanf <jeanfvoyer@gmail.com>
2022-02-18 15:35:48 -0500

    Ajout du temps ressenti dans le résultat

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -11,6 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+    messageThree.textContent = ''
 
     fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
@@ -18,7 +20,8 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error
             }else{
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                messageTwo.textContent = data.forecast.weather_descriptions[0]
+                messageThree.textContent = 'Temps ressenti ' + data.forecast.feelslike
             }
         })
     })
